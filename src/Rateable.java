e60f36f3841c6459156df3d80dca4259be0d12d7
s33721 <s33721@pjwstk.edu.pl>
2025-05-23 16:19:26 +0200

    wrote implementation for rateable interface

--- src/Rateable.java
+++ src/Rateable.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public interface Rateable {
+    ArrayList<Integer> ratings = new ArrayList<Integer>();
+    void addRating(int rating);
+    double getAverageRating();
 }
