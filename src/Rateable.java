0e3ab296319b9b340a8b05dedde1c6aeaeba8e3d
s33721 <s33721@pjwstk.edu.pl>
2025-05-23 16:35:38 +0200

    created movie class

--- src/Rateable.java
+++ src/Rateable.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 
 public interface Rateable {
     ArrayList<Integer> ratings = new ArrayList<Integer>();
+
     void addRating(int rating);
+
     double getAverageRating();
 }
