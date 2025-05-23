acc3355a3cd015070984e2fc7f7c5dc65681ff22
s33721 <s33721@pjwstk.edu.pl>
2025-05-23 16:17:07 +0200

    wrote implementation for Rentable interface

--- src/Rentable.java
+++ src/Rentable.java
@@ -1,2 +1,9 @@
 public interface Rentable {
+    int MAX_RENTAL_DAYS = 14;
+    static double calculateLateFee(int daysLate){
+        return daysLate * 0.5;
+    }
+    void checkAvailability();
+    boolean rentItem();
+    boolean returnItem();
 }
